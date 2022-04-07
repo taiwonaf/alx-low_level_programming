5a3ceb28378c44b6d525f52a9da171a3110d8de0
taiwo <nafiutaiwosolomon@gmail.com>
2022-04-07 15:13:13 -0700

    Tasks comoleted

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+*main - function begins
+*
+* Return: function ends
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
