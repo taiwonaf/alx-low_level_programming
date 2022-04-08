3388e119dc725d411e82c55da7ad156ed42b0634
taiwo <nafiutaiwosolomon@gmail.com>
2022-04-08 14:41:56 -0700

    Task 1 line wc changed from 45 to 44 with space removal

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ int main(void)
 	printf("Last digit of %d is %d and is ", n, last_digit);
 	if (last_digit > 5)
 	{
-		printf("greater than 5 \n");
+		printf("greater than 5\n");
 	}
 	else if (last_digit  == 0)
 	{
