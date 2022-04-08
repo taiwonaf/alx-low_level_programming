aff45f73acbe5d32b3dabe5670433fe2e7536103
taiwo <nafiutaiwosolomon@gmail.com>
2022-04-08 14:10:02 -0700

    Task 4 changed

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
+
 /**
-  * main 
-  *
-  * Return: Always (Success)
-  */
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char c;
@@ -11,11 +11,8 @@ int main(void)
 	for (c = 'a'; c <= 'z'; c++)
 	{
 		if (c != 'e' && c != 'q')
-		{
-			putchar(c);
-		}
+		putchar(c);
 	}
 	putchar('\n');
-
 	return (0);
 }
