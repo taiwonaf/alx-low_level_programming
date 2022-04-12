c010635a77ec6f4bb3891e2a222f21e692c865d4
taiwo <nafiutaiwosolomon@gmail.com>
2022-04-12 11:17:45 -0700

    First task 0. completed

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main-main block
+ *Description:print -putchar in a new line
+ *Return:this program returns zero
+ */
+int main(void)
+{
+    printf("_putchar\n");
+    return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is a readme file for the functions and nested loops tasks
