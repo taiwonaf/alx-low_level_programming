f16a8cb4fc3ec03efb223f7f44595411be6fc57c
taiwo <nafiutaiwosolomon@gmail.com>
2022-04-12 12:13:47 -0700

    Task 1 to print alphabet in lower case using char a to Z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ */
+void print_alphabet(void)
+{
+	char alphabet;
+
+	for (aphabet = 'a'; alphabet <= 'z': letter++) 
+	{
+		_putchar(alphabet);
+		_putchar('\n')
+	}
+	return(0);
+}
