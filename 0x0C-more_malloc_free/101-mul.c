4a39aa90170c72b00671583cecddc92e871c6d75
taiwo <nafiutaiwosolomon@gmail.com>
2022-05-09 16:05:51 -0700

    correction

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,90 +1,83 @@
-#include "main.h"
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
-
+#include "main.h"
+#include <stddef.h>
+int check_error(char **argv, int argc);
 /**
- * _puts - prints a string, follwed by a new line
- * @str: pointer to the string to print
- * 
- * Return: void
+ * main - multiplies two positive numbers
+ * @argc: n arguments
+ * @argv: args
+ * Return: int
  */
 
-void _puts(char *str)
+int main(int argc, char **argv)
 {
-	int i = 0;
-	while (str[i])
-	{
-		_putchar(str[i]);
-		i++;
-	}
+if (argc != 3)
+{
+printf("Error\n");
+exit(98);
+}
+chkk = check_error(argv, argc);
+if (chkk == 0)
+{
+l1 = strlen(argv[1]);
+l2 = strlen(argv[2]);
+for (i = l1 - 1, j = 0; i >= 0; i--, j++)
+{
+num1[j] = argv[1][i] - '0';
+}
+for (i = l2 - 1, j = 0; i >= 0; i--, j++)
+{
+num2[j] = argv[2][i] - '0';
+}
+for (i = 0; i < l2; i++)
+{
+for (j = 0; j < l1; j++)
+{
+mul[i + j] += num2[i] * num1[j];
+}
+}
+for (i = 0; i < l1 + l2; i++)
+{
+tmp = mul[i] / 10;
+mul[i] = mul[i] % 10;
+mul[i + 1] = mul[i + 1] + tmp;
+}
+for (i = l1 + l2; i >= 0; i--)
+{
+if (mul[i] > 0)
+break;
+}
+for (; i >= 0; i--)
+{
+printf("%d", mul[i]);
+}
 }
 
-/**
- * _atoi - convert a string to an integer
- * @s: char string
- * Return: integer converted
- */
 
-int _atoi(const char *s)
-{
-	int sign = 1;
-	unsigned long int resp = 0, firstNum, i;
-
-	for (firstNum = 0; (s[firstNum] < 48 && s[firstNum] > 57); firstNum++)
-	{
-		if (s[firstNum] == '-')
-		{
-			sign *= -1;
-		}
-	}
-	for (i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
-	{
-		resp *= 10;
-		resp += s[i] - 48;
-	}
-
-	return (sign * resp)
+printf("\n");
+return (0);
 }
 
+
 /**
- * print_int -prints an integer
- * @n: int
- *
- * Return: 0
+ * check_error - check error
+ * @argc: n arguments
+ * @argv: args
+ * Return: int
  */
-
-void print_int(unsigned long int n)
+int check_error(char **argv, int argc)
 {
-	unsigned long int divisor = 1, i, resp;
-
-	for (i = 0; n / divisor > 9; i++, divisor *= 10)
-		;
-	for (; divisor >= 1; n %= divisor, divisor /= 10)
-	{
-		resp = n / divisor;
-		_putchar('0' + resp);
-	}
+int i, j;
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (argv[i][j] > 57 || argv[i][j] < 48)
+{  printf("Error\n");
+exit(98); }
 }
-
-/**
- * main - print the result of the multiplication 
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: 0
- */
-
-int main(int argc, char const *argv[])
-{
-	(void)argc;
-
-	if (argc != 3)
-	{
-		_puts("Error ");
-		exit(98);
-	}
-	print_int(_atoi(argv[1]) * _atoi(argv[2]));
-	_putchar('\n');
-
-	return (0);
+}
+return (0);
 }
